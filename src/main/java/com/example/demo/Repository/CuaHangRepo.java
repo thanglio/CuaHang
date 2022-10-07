d07d9dbb1a35739b8b31e2a5e11031380745f904
thanglio <thangnobitalio@gmail.com>
2022-10-07 11:29:16 +0700

    git commit -m"halo"

---
+++ src/main/java/com/example/demo/Repository/CuaHangRepo.java
@@ -0,0 +1,7 @@
+package com.example.demo.Repository;
+
+import com.example.demo.Entity.CuaHang;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CuaHangRepo extends JpaRepository<CuaHang,Long> {
+}
