a796bf7a5df9fa549448b2390b26146c99470fc8
thanglio <thangnobitalio@gmail.com>
2022-10-07 12:15:01 +0700

    "cua hang"

--- src/main/java/com/example/demo/Controller/CuaHangController.java
+++ src/main/java/com/example/demo/Controller/CuaHangController.java
@@ -1,9 +1,18 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Service.ImplementService.CuaHangServiceImplement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping()
+@RequestMapping("/api/cuahang")
 public class CuaHangController {
+    @Autowired
+    CuaHangServiceImplement cuaHangServiceImplement
+    @DeleteMapping
+    public void deleteCuaHang(Long id){
+    cuaHangServiceImplement.deleteCuaHang(id);
+}
 }
--- src/main/java/com/example/demo/Controller/KhachHangController.java
+++ src/main/java/com/example/demo/Controller/KhachHangController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/khachhang")
 public class KhachHangController {
     @Autowired
     KhachHangServiceImplement khachHangServiceImplement;
